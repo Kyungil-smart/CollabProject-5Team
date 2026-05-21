8cc053d49335daca4f7e1453fa5519dfffbc339a
Nahyunsusu <85683426+Nahyunsusu@users.noreply.github.com>
2026-05-21 23:55:18 +0900

    [Feat] Employee 매니저 추가

--- CollabProject-5Team/Assets/Project/Scripts/NHS/Employee/HaveEmployees.cs
+++ CollabProject-5Team/Assets/Project/Scripts/NHS/Employee/HaveEmployees.cs
@@ -2,23 +2,19 @@ using UnityEngine;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 
-public class HaveEmployees : MonoBehaviour
+public class HaveEmployees
 {
      public List<Employee>  HaveEmployeeList => _haveEmployeeList;
     private List<Employee> _haveEmployeeList = new List<Employee>();
 
     private Stats _totalStat;
 
-    void Start()
-    {
-    }
-
     public void AddEmployee(int index)
     {
 
     }
 
-    public Stats AllStats()
+    public Stats GetAllStats()
     {
         return _totalStat;
     }
