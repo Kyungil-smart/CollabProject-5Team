292341de568432f914c67940bc385cc8e69084a8
Masttte <peonyak08@gmail.com>
2026-06-01 18:26:56 +0900

    chore: 구글시트 에디터 메뉴에 등록

--- CollabProject-5Team/Assets/Settings/Editor/GoogleSheetEditorMenu.cs
+++ CollabProject-5Team/Assets/Settings/Editor/GoogleSheetEditorMenu.cs
@@ -2,13 +2,13 @@ using UnityEditor;
 
 public static class GoogleSheetEditorMenu
 {
-    [MenuItem("구글시트/Test Sheet 1")]
+    [MenuItem("구글시트/대화 Pool")]
     private static void LoadSheet1()
     {
         EditorUtility.OpenPropertyEditor(DataRequestSet.Get(1));
     }
 
-    [MenuItem("구글시트/Test Sheet 2")]
+    [MenuItem("구글시트/대화 Node")]
     private static void LoadSheet2()
     {
         EditorUtility.OpenPropertyEditor(DataRequestSet.Get(2));
