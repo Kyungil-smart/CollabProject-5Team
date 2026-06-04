a78e5bbfbfbab7935d351f297bcfe0cfbd47830b
Masttte <peonyak08@gmail.com>
2026-06-05 08:59:03 +0900

    Update Quest.cs

--- CollabProject-5Team/Assets/Project/Scripts/Company/Data/Quest.cs
+++ CollabProject-5Team/Assets/Project/Scripts/Company/Data/Quest.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public enum QuestStatus
 {
     None,      // 없음
-    Available, // 수락 가능
     Playing,   // 진행 중
     Completed, // 완료
     Failed,    // 실패
@@ -16,7 +15,18 @@ public enum QuestType
     Event,  // 이벤트 퀘스트
 }
 
+public enum QuestMode
+{
+    None,
+    Tap,   // 연타
+    Hold,  // 누르고 있기
+    Swipe, // 슬라이드
+    Talk,
+}
+
 public class Quest
 {
-    
+    public QuestStatus status;
+    public QuestType type;
+    public QuestMode mode;
 }
