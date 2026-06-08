40885d0b3c21689cd702c3bd923d640e09ba1a64
Masttte <peonyak08@gmail.com>
2026-06-08 21:50:26 +0900

    chore: 주.석.처.리

--- CollabProject-5Team/Assets/Project/Scripts/UI/View/HRView.cs
+++ CollabProject-5Team/Assets/Project/Scripts/UI/View/HRView.cs
@@ -144,7 +144,7 @@ namespace GameDevTycoon.UI.Ingame
 
         private void Awake()
         {
-            _hrPopup.SetActive(false);
+            //_hrPopup.SetActive(false);
 
             // Tab_EmployeeManage를 기본 탭으로
             ShowTab(HRTab.EmployeeManage);
