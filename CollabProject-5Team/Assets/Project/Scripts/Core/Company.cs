829ec6cfc5ade776ccac7f052c0479a30bbc5cc2
4awol <skgustn3355@gmail.com>
2026-06-23 11:25:36 +0900

    [Update] 입력과 대사 연동

--- CollabProject-5Team/Assets/Project/Scripts/Core/Company.cs
+++ CollabProject-5Team/Assets/Project/Scripts/Core/Company.cs
@@ -11,6 +11,7 @@ public class Company : MonoBehaviour
     public GameObject[] projectPrefab; // 소형, 중형, 대형 순서 (Project 컴포넌트 포함)
 
     [Header("회사 정보")]
+    public string playerName;
     public string Name;
     public ReactiveProperty<int> gold = new(10000); // 보유 자금
     public int level = 1;                           // 회사 레벨, 회사 증축 상황(소형=1 중형=2 대형=3) 과 같음
--- CollabProject-5Team/Assets/WIP/NHS/CutSceen/Newgame.cs
+++ CollabProject-5Team/Assets/WIP/NHS/CutSceen/Newgame.cs
@@ -56,12 +56,6 @@ public class Newgame : MonoBehaviour
 
         if (currentData != null)
         {
-            if (currentData.id == 1000000)
-            {
-                OpenPlayerNamePanel();
-                return;
-            }
-
             string msg = currentData.dialogue
                 .Replace("[Company]", _companyName)
                 .Replace("[Player]", _playerName);
@@ -73,20 +67,23 @@ public class Newgame : MonoBehaviour
             _cutSceneUI.characterName.text = name;
             _cutSceneUI.dialogue.text = msg;
             _cutSceneUI.image.sprite = currentData.cutSceenImage;
+
+            if (currentData.id == 1000040)
+            {
+                OpenPlayerNamePanel();
+                return;
+            }
         }
     }
 
     private void OpenPlayerNamePanel()
     {
+          _cutSceneUI.panel.SetActive(false);
         _setPlayerNamePanel.SetActive(true);
-        
-        _cutSceneUI.nextButton.interactable = false; 
     }
 
     private void OnNextDialogueClicked()
     {
-        if (_setPlayerNamePanel.activeSelf) return;
-
         _currentIdx++;
         ShowCutScene();
     }
@@ -97,6 +94,8 @@ public class Newgame : MonoBehaviour
         if (string.IsNullOrWhiteSpace(input)) return;
 
         _companyName = input;
+        Company.Instance.Name = _companyName;
+
         _setCompanyPanel.SetActive(false);
 
         // 컷신 패널을 켜고 첫 컷신 시작
@@ -110,9 +109,11 @@ public class Newgame : MonoBehaviour
         if (string.IsNullOrWhiteSpace(input)) return;
 
         _playerName = input;
+        Company.Instance.playerName = _playerName;
+
         _setPlayerNamePanel.SetActive(false);
-        
-        _cutSceneUI.nextButton.interactable = true;
+
+        _cutSceneUI.panel.SetActive(true);
 
         _currentIdx++;
         ShowCutScene();
