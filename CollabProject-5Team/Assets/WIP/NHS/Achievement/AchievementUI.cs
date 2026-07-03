10567792ca2ca3bff536c292f717587fe6f593dc
4awol <skgustn3355@gmail.com>
2026-07-03 15:18:06 +0900

    [Update] 튜토리얼 씬 수정

---
+++ CollabProject-5Team/Assets/WIP/NHS/Achievement/AchievementUI.cs
@@ -0,0 +1,63 @@
+using TMPro;
+using UnityEngine;
+using DG.Tweening;
+using Cysharp.Threading.Tasks;
+
+
+public class AchievementUI : MonoBehaviour
+{
+    [SerializeField] private RectTransform _panelRect;
+
+    [SerializeField] private TextMeshProUGUI _textTitle;
+    [SerializeField] private TextMeshProUGUI _textDetail;
+
+    [Header("연출 설정")]
+    [SerializeField] private float _moveX        = 450f; // 이동할 X 거리
+    [SerializeField] private float _moveDuration = 0.5f; // 들어오고 나가는 시간
+    [SerializeField] private float _showDuration = 2.0f; // 화면에 머무르는 시간
+
+    private Vector2  _startPosition;
+    private Sequence  _showSequence;
+
+    private void Awake()
+    {
+        _startPosition = _panelRect.anchoredPosition;
+        HideImmediately();
+    }
+
+    private void OnDestroy()
+    {
+        if (_showSequence != null)
+        {
+            _showSequence.Kill();
+        }
+    }
+
+    public void ShowAchievement(string title, string description)
+    {
+         _textTitle.text = title;
+        _textDetail.text = description;
+
+        if (_showSequence != null && _showSequence.IsActive())
+        {
+            _showSequence.Kill();
+        }
+
+        HideImmediately();
+
+        _showSequence = DOTween.Sequence();
+
+        _showSequence
+            .Append(_panelRect.DOAnchorPos(_startPosition, _moveDuration).SetEase(Ease.OutBack))
+            .AppendInterval(_showDuration)
+            .Append(_panelRect.DOAnchorPos(new Vector2(_startPosition.x + _moveX, _startPosition.y), _moveDuration).SetEase(Ease.InBack))
+            .OnComplete(() =>
+            {
+            });
+    }
+
+    private void HideImmediately()
+    {
+        _panelRect.anchoredPosition = new Vector2(_startPosition.x - _moveX, _startPosition.y);
+    }
+}
