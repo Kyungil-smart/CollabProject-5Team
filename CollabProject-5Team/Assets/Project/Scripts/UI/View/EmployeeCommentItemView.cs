51f5fc6452ccdb638d1b8b16fc0ae8bc42e06f7c
Masttte <peonyak08@gmail.com>
2026-07-06 10:04:32 +0900

    fix: 직원별 코멘트 충성도 수치 표기

--- CollabProject-5Team/Assets/Project/Scripts/UI/View/EmployeeCommentItemView.cs
+++ CollabProject-5Team/Assets/Project/Scripts/UI/View/EmployeeCommentItemView.cs
@@ -53,30 +53,29 @@ namespace GameDevTycoon.UI.Ingame
             _tagLabel.text = RoleToString(so.role);
             _tagBG.color = RoleToColor(so.role);
 
-            SetLoyaltyChange(mutable.loyalty - mutable.preLoyalty);
+            SetLoyaltyChange(mutable.loyalty, mutable.loyalty - mutable.preLoyalty);
             SetComment(employee, commentText);
         }
 
-        private void SetLoyaltyChange(int delta)
+        private void SetLoyaltyChange(int currentLoyalty, int delta)
         {
             _loyaltyChangeTitleLabel.text = "충성도";
+            _loyaltyChangeValue.text = currentLoyalty.ToString();
+            _loyaltyChangeValue.color = Color.black;
 
             if (delta > 0)
             {
-                _loyaltyChangeValue.text = $"+{delta}";
-                _loyaltyChangeValue.color = ColorUp;
+                _loyaltyChangeWeightLabel.text = $"+{delta}";
                 _loyaltyChangeWeightLabel.color = ColorUp;
             }
             else if (delta < 0)
             {
-                _loyaltyChangeValue.text = delta.ToString();
-                _loyaltyChangeValue.color = ColorDown;
+                _loyaltyChangeWeightLabel.text = delta.ToString();
                 _loyaltyChangeWeightLabel.color = ColorDown;
             }
             else
             {
-                _loyaltyChangeValue.text = "-";
-                _loyaltyChangeValue.color = ColorNone;
+                _loyaltyChangeWeightLabel.text = "-";
                 _loyaltyChangeWeightLabel.color = ColorNone;
             }
         }
