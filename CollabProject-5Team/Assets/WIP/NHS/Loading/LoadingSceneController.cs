4b3d14fea1e664e14e5c23ceb4f4d73981908bc2
Masttte <peonyak08@gmail.com>
2026-07-09 12:24:06 +0900

    fix:게임씬 넘어갈떈 비밀 출근중 적용

--- CollabProject-5Team/Assets/WIP/NHS/Loading/LoadingSceneController.cs
+++ CollabProject-5Team/Assets/WIP/NHS/Loading/LoadingSceneController.cs
@@ -1,18 +1,30 @@
 using Cysharp.Threading.Tasks;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LoadingSceneController : MonoBehaviour
 {
+    [SerializeField] Image _loadingImage;
+    [SerializeField] Sprite _gameSceneSprite;
+
     public static string NextSceneName { get; set; }
 
     private void Start()
     {
+        ApplySceneSprite();
+
         // Start에서 바로 UniTask 실행
         LoadTargetSceneAsync().Forget();
     }
+
+    private void ApplySceneSprite()
+    {
+        if (NextSceneName != "GameScene") return;
+
+        _loadingImage.sprite = _gameSceneSprite;
+    }
+
     private async UniTaskVoid LoadTargetSceneAsync()
     {
         var op = SceneManager.LoadSceneAsync(NextSceneName);
